6ef0ed94d6b4ba0235457166d6c8f3caf5a991fb
kevin Chan <kevin.chan@ringus-solution.com>
2021-12-14 19:06:13 +0800

    half way done post comments

---
+++ microservices/blog/comments/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { randomBytes } from 'crypto'
+
+const app = express();
+app.use(express.json());
+
+
+const commentsByPostId = {} as Record<string,{id:string,content:string}[]>;
+
+app.get('/posts/:id/comments', (req,res) => {
+    res.send(commentsByPostId[req.params.id] || [])
+});
+
+app.post('/posts/:id/comments', (req,res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({id:commentId, content});
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.send(comments)
+});
+
+app.listen(4001, ()=> {
+    console.log("Comments service is listening at http://localhost:4001");
+    
+})
