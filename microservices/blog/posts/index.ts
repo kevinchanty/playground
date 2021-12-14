f93abdca75863ed5ce6e546686e24bcd37f7c751
kevin Chan <kevin.chan@ringus-solution.com>
2021-12-14 18:26:51 +0800

    post service done

---
+++ microservices/blog/posts/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { randomBytes } from 'crypto';
+
+const app = express();
+app.use(express.json())
+type Post = {
+    id:string
+    title:string
+}
+
+const posts:Record<string, Post> = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body
+
+    posts[id] = {
+        id,
+        title,
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log("Posts service is listening on localhost:4000")
+})
