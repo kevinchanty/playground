e57a0d5d0754ccc4683ea78d22c915b8c22b62b3
Kevin Chan <kevin.chan@ringus-solution.com>
2021-12-22 16:57:05 +0800

    bootstrap and postcreate

--- microservices/blog/client/src/App.tsx
+++ microservices/blog/client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import PostCreate from './components/PostCreate';
 
 function App() {
   return (
-    <div className="App">
-          Learn React
+    <div className="App container">
+          <h1>Create Post</h1>
+          <PostCreate/>
     </div>
   );
 }
