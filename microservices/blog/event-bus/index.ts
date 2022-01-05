f50c3a052ad78563dc59ede74159ccd76d38fae3
Kevin Chan <kevin.chan@ringus-solution.com>
2022-01-05 17:07:36 +0800

    event bys storing event

--- microservices/blog/event-bus/index.ts
+++ microservices/blog/event-bus/index.ts
@@ -5,8 +5,12 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 
-app.post('/events', (req: Request<core.ParamsDictionary, any, {}>, res) => {
+const events = [] as {type:string, data:any}[];
+
+app.post('/events', (req: Request<core.ParamsDictionary, any, {type:string, data:any}>, res) => {
     const event = req.body;
+
+    events.push(event);
 
     try {
         axios.post('http://localhost:4000/events', event);
@@ -18,6 +22,10 @@ app.post('/events', (req: Request<core.ParamsDictionary, any, {}>, res) => {
     }
         
     res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.json(events);
 });
 
 app.listen(4005, () => {
