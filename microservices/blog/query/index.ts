614a57a781e2f35d60a82acf7231f8e29558dede
Kevin Chan <kevin.chan@ringus-solution.com>
2022-01-10 17:52:44 +0800

    index

--- microservices/blog/query/index.ts
+++ microservices/blog/query/index.ts
@@ -65,7 +65,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
     console.log("Query Services Listening to 4002.")
 
-    const res = await axios.get('http://localhost:4005/events');
+    const res = await axios.get('http://event-bus-srv:4005/events');
 
     for (const event of res.data) {
         console.log('Processing event:', event.type);
