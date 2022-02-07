16420eb280dc00d94f69e58ec73cbfe31382fe7a
Kevin Chan <kevin.chan@ringus-solution.com>
2022-02-07 18:06:34 +0800

    Flutter codelab

--- dart/hello_world.dart
+++ dart/hello_world.dart
@@ -1,3 +1,24 @@
-void main() {
+import 'dart:io';
+
+main() async {
   print('Hello, World!');
+  getEnvInfo();
+}
+
+writeFile() async {
+  final quotes = File('quotes.txt');
+  const stronger = 'That which does not kill you';
+
+  await quotes.writeAsString(stronger, mode: FileMode.append);
+}
+
+getEnvInfo() {
+  final envVarMap = Platform.environment;
+
+  print(Platform.isMacOS);
+  print(Platform.script);
+
+  print('PWD = ${envVarMap['PWD']}');
+  print('LOGNAME = ${envVarMap['LOGNAME']}');
+  print('PATH = ${envVarMap['PATH']}');
 }
